36c93d03549bd2e280a993f3f5cf3ebfcafe48ef
jesperancinha <jofisaes@gmail.com>
2024-09-16 20:53:33 +0200

    First simple project

---
+++ Makefile
@@ -0,0 +1,7 @@
+install-spring-boot-cli:
+	brew tap spring-cli-projects/spring-cli
+	brew install spring-cli
+install-home-brew:
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+create-new-service:
+	spring boot new --from ai --name oga-simple-service
---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "org.jesperancinha.oga"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
